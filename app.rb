249fb1b217c56ecea9245519f11c31069e80dd25
Maddy Lau <maddylau@gmail.com>
2015-01-20 10:51:37 -0500

    adding app, schema, models and views.

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
+require 'bundler/setup'
+require 'rack-flash'
 
 set :database, "sqlite3:app.sqlite3"
+set :sessions, true
+enable :sessions
+use Rack::Flash, :sweep => true
+
+get '/' do
+	erb :home
+end
+
+
+post '/' do
+	@user = User.where(:email_address => params[:email_address]).first
+	if @user.password == params[:password]
+		flash[:notice] = "Hello, #{@user.email_address}!"
+	else
+		redirect "/"
+	end
+end
+
